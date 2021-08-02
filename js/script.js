a030967c124a1423e291e91061ffa85fc7f543d9
junaiyet <junaiyetmia470@gmail.com>
2021-08-02 23:25:31 +0600

    northendlab

--- js/script.js
+++ js/script.js
@@ -75,3 +75,13 @@ $(window).scroll(function () {
       $('.filtr-container').filterizr();
     }
 
+// ////////////////////
+let dropdownBtn = document.querySelector('.menu-btn');
+let menuContent = document.querySelector('.menu-content');
+dropdownBtn.addEventListener('click',()=>{
+   if(menuContent.style.display===""){
+      menuContent.style.display="block";
+   } else {
+      menuContent.style.display="";
+   }
+})
